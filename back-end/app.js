4e24d770cd6efe9c167d7cadfa3482a09683e350
stainednuttz <stianhazel@gmail.com>
2021-08-03 15:17:44 +0100

    port env var

--- back-end/app.js
+++ back-end/app.js
@@ -42,5 +42,5 @@ app.use((error, req, res, next) => {
 
 // connect to mongoDB
 mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ek8qv.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
-  .then(() => app.listen(5000))
+  .then(() => app.listen(process.env.PORT || 5000))
   .catch(err => console.log(err));
