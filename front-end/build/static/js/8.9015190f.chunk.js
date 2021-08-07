da783f5be622a655adfbdff2d28952f5744f187f
stainednuttz <stianhazel@gmail.com>
2021-08-07 19:19:50 +0100

    npm scripts 2

--- front-end/build/static/js/8.9015190f.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpmern-ecommerce"]=this["webpackJsonpmern-ecommerce"]||[]).push([[8],{60:function(e,c,r){"use strict";r.r(c);r(0);var n=r(1);c.default=function(){return Object(n.jsx)("div",{children:"Explore"})}}}]);
-//# sourceMappingURL=8.9015190f.chunk.js.map
